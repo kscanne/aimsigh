931a5042a5f0d42486ce2d6bc82800a32d235015
kscanne <kscanne@gmail.com>
2005-11-06 12:40:38 +0000

    syntax errors; also sort highest to lowest!
    git-svn-id: file:///home/kps/seal/gitemp/aimsigh/trunk@38 a6873ebf-e95b-465c-b432-0e6dd3dc36f7

--- prsort.pl
+++ prsort.pl
@@ -5,9 +5,10 @@ use warnings;
 my $crub = '/usr/local/share/crubadan/ga';
 
 my %prhash;
-while my $docid (<STDIN>) {
-	chomp $docid;
-	open (SONRAI, "<", "$crub/sonrai/$docid.dat") or die "Could not open $docid.dat file\n";
+while (<STDIN>) {
+	chomp;
+	my $docid = $_;
+	open (SONRAI, "<", "$crub/sonrai/$docid.dat") or die "Could not open $docid.dat file: $!\n";
 	while (<SONRAI>) {
 		chomp;
 		if (m/^pagerank: /) {
@@ -17,5 +18,5 @@ while my $docid (<STDIN>) {
 	}
 	close SONRAI;
 }
-print "$_\n" foreach (sort {$prhash{$a} <=> $prhash{$b}} keys $prhash);
+print "$_\n" foreach (sort {$prhash{$b} <=> $prhash{$a}} keys %prhash);
 exit 0;
