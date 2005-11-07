f34c6f97236575f2572b1ec1114585d1ac1b56f8
kscanne <kscanne@gmail.com>
2005-11-07 04:21:55 +0000

    used in swish-e indexing
    git-svn-id: file:///home/kps/seal/gitemp/aimsigh/trunk@41 a6873ebf-e95b-465c-b432-0e6dd3dc36f7

---
+++ swishy.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+# reads a list of docId's on stdin that have been sorted by PageRank
+my $base="/snapshot/aimsigh/$ARGV[0]";
+my $count=100000;
+while (<STDIN>) {
+	chomp;
+	my $docId=$_;
+	open (FOINSE, "<", "$base/$docId.txt") or die "Could not open $docId: $!\n";
+	my $dum=<FOINSE>;   # <DOC>
+	$dum=<FOINSE>;      # <DOCNO>...
+	my $doc='';
+	while (<FOINSE>) {
+		$doc .= $_ unless /^<\/DOC>$/;
+	}
+	my $size = length $doc;
+	print "Path-Name: $count-pr\n";
+	print "Content-Length: $size\n";
+	print "Document-Type: TXT*\n";
+	print "\n";
+	print $doc;
+	$count++;
+}
+closedir DIRH;
+exit 0;
