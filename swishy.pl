aa5d33a5b42f6db7a1aebcbe7870d902e25a94de
kscanne <kscanne@gmail.com>
2005-11-07 05:51:47 +0000

    TEIDIL case.
    git-svn-id: file:///home/kps/seal/gitemp/aimsigh/trunk@45 a6873ebf-e95b-465c-b432-0e6dd3dc36f7

--- swishy.pl
+++ swishy.pl
@@ -5,23 +5,43 @@ use warnings;
 
 # reads a list of docId's on stdin that have been sorted by PageRank
 my $base="/snapshot/aimsigh/$ARGV[0]";
+my $ext='txt';
+if ($ARGV[0] eq 'TEIDIL') {
+	$base="/usr/local/share/crubadan/ga/sonrai";
+	$ext='dat';
+}
 my $count=100000;
 while (<STDIN>) {
 	chomp;
 	my $docId=$_;
-	open (FOINSE, "<", "$base/$docId.txt") or die "Could not open $docId: $!\n";
-	my $dum=<FOINSE>;   # <DOC>
-	$dum=<FOINSE>;      # <DOCNO>...
 	my $doc='';
-	while (<FOINSE>) {
-		$doc .= $_ unless /^<\/DOC>$/;
+	open (FOINSE, "<", "$base/$docId.$ext") or die "Could not open $docId: $!\n";
+	if ($ARGV[0] eq 'TEIDIL') {
+		while (<FOINSE>) {
+			if (/^title: /) {
+				chomp;
+				($doc) = m/^title: (.*)$/;
+				$doc='' if ($doc eq 'Gan teideal');
+				$doc =~ s/^(([^ ]+ +){7}).*$/$1/;
+			}
+		}
 	}
+	else {
+		my $dum=<FOINSE>;   # <DOC>
+		$dum=<FOINSE>;      # <DOCNO>...
+		while (<FOINSE>) {
+			$doc .= $_ unless /^<\/DOC>$/;
+		}
+	}
+	close FOINSE;
 	my $size = length $doc;
-	print "Path-Name: $count-$docId\n";
-	print "Content-Length: $size\n";
-	print "Document-Type: TXT*\n";
-	print "\n";
-	print $doc;
-	$count++;
+	if ($size > 0) {
+		print "Path-Name: $count-$docId\n";
+		print "Content-Length: $size\n";
+		print "Document-Type: TXT*\n";
+		print "\n";
+		print $doc;
+		$count++;
+	}
 }
 exit 0;
